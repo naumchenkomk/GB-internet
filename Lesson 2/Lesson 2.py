7c3a896f7a8724ff9dfad25f774a0bb4eab4ec98
naumchenkomk <naumchenkomk@gmail.com>
2021-03-17 01:25:56 +0300

    Lesson 2 Добавила sleep, exception и выбор страниц поиска пользователем

--- Lesson 2/Lesson 2.py
+++ Lesson 2/Lesson 2.py
@@ -17,6 +17,8 @@ from fake_useragent import UserAgent
 u = UserAgent().random
 
 
+# Сохранение результата запроса к HH в файл (0) - pickle
+
 def save_pickle(obj, path):
     with open(path, 'ab+') as add_file:
         pickle.dump(obj, add_file)
@@ -32,7 +34,7 @@ def get(url, headers, params, proxies):
     return req
 
 
-# Сохранение результата запроса к HH в файл (1)
+# Сохранение результата запроса к HH в файл (1) - проверка на то, что страница является последней
 
 def is_last_page(request):
     soup_incr = bs(request.text, 'html.parser')
@@ -43,54 +45,55 @@ def is_last_page(request):
 # Сохранение результата запроса к HH в файл (2)
 
 def hh_request(word, page_nbr, f_name):
-    path_inc = f"{f_name}_inc.rsp"
-    # очищаем файл для записи одной страницы
-    f = open(path_inc, 'w')
-    f.close()
-    # Браузер
-    ua = UserAgent().random
+    for i in range(10):
+        try:
+            path_inc = f"{f_name}_inc.rsp"
+            # очищаем файл для записи одной страницы
+            f = open(path_inc, 'w')
+            f.close()
+            # Браузер
+            ua = UserAgent().random
 
-    # while True:
-    url = "https://hh.ru/search/vacancy"
-    params = {
-        'L_is_autosearch': 'false',
-        'clusters': 'true',
-        'enable_snippets': 'true',
-        'text': word,
-        'page': page_nbr
-    }
-    headers = {
-        'User-Agent': ua
-    }
-    proxies = {
-        'http': 'http://3.88.169.225:80',
-        # 'https': 'https://165.227.223.19:3128',
-    }
-    answ = get(url, headers, params, proxies)
-    if answ.status_code != 200:
-        raise Exception
-        # break
-    print('Статус запроса к текущей странице:', answ)
+            url = "https://hh.ru/search/vacancy"
+            params = {
+                'L_is_autosearch': 'false',
+                'clusters': 'true',
+                'enable_snippets': 'true',
+                'text': word,
+                'page': page_nbr
+            }
+            headers = {
+                'User-Agent': ua
+            }
+            proxies = {
+                'http': 'http://3.88.169.225:80',
+                # 'https': 'https://165.227.223.19:3128',
+            }
+            answ = get(url, headers, params, proxies)
+            answ.encoding = 'utf-8'
+            print('Статус запроса к текущей странице:', answ)
 
-    save_pickle(answ, path_inc)
-    save_pickle(answ, f"{f_name}.rsp")
+            if answ.status_code != 200:
+                raise Exception
 
-    # print("Страница", page_id, "Последняя страница? ", is_last_page(answ))
-    return answ
-    # if is_last_page(answ):
-    #     return answ
-    #     break
-    # else:
-    #     page_id = page_id + 1
-    #     continue
+            save_pickle(answ, path_inc)
+            save_pickle(answ, f"{f_name}.rsp")
+
+            return answ
+        except Exception as e:
+            if answ.status_code == 404:
+                print('Задана несуществующая страница. Попробуйте еще раз.')
+                return answ.status_code
+            else:
+                print(e, 'попытка №', i)
+                time.sleep(0.5 + random.random())
 
 
-# Сохранение результата запроса к HH в файл (3)
-# sleep
+# парсинг файла с результатом запроса к HH (1) - зарплата
 
-# парсинг файла с результатом запроса к HH
 def zp_pars(sum):
     sum = sum.replace('\xa0', '')
+    sum = sum.replace('\u202f', '')
     if sum.find('от ') != -1:
         sum_min = int(sum[sum.find(' ') + 1:sum.rfind(' ')])
         sum_max = None
@@ -104,7 +107,7 @@ def zp_pars(sum):
     return [sum_min, sum_max, curr]
 
 
-# Парсинг одной страницы HH (одного документа HTML)
+# парсинг файла с результатом запроса к HH (2) - Парсинг одной страницы HH (одного документа HTML)
 
 def hh_parser(f_name_in, f_name_out):
     req = load_pickle(f"{f_name_in}_inc.rsp")
@@ -121,13 +124,13 @@ def hh_parser(f_name_in, f_name_out):
         # Ссылка на саму вакансию
         link = i_tag.find(attrs={"class": "resume-search-item__name"}).a['href']
         # Порядковый номер вакансии в результате запроса
-        # poz = i_tag.find(attrs={"class": "resume-search-item__name"}).a['data-position']
+        poz = i_tag.find(attrs={"class": "resume-search-item__name"}).a['data-position']
         # Сайт, откуда собрана вакансия
         resource = 'https://hh.ru/'
 
         info['vacancy_name'] = vacancy_name
         info['link'] = link
-        # info['poz'] = poz
+        info['poz'] = poz
         info['resource'] = resource
 
         # vacancies.append(info)
@@ -146,10 +149,12 @@ def hh_parser(f_name_in, f_name_out):
         hh_vacancies.append(info)
 
     with open(f"{f_name_out}_inc.json", "w+") as f:
-        json.dump(hh_vacancies, f, indent=2, ensure_ascii=False)
+        json.dump(hh_vacancies, f, indent=2) #, ensure_ascii=False)
 
     return hh_vacancies
 
+
+# парсинг файла с результатом запроса к HH (3) - сохранение в json-файл результата работы парсера
 
 def json_add_to_file(f_name, page):
     with open(f"{f_name}_inc.json", "r") as f:
@@ -158,68 +163,66 @@ def json_add_to_file(f_name, page):
 
     if page == 0:
         with open(f"{f_name}.json", "w+") as f:
-            json.dump(file, f, indent=2, ensure_ascii=False)
+            json.dump(file, f, indent=2) #, ensure_ascii=False)
     else:
         with open(f"{f_name}.json", "r") as f:
             to_file_total = json.load(f)
             file = to_file_total + to_file
         with open(f"{f_name}.json", "w+") as f:
-            json.dump(file, f, indent=2, ensure_ascii=False)
+            json.dump(file, f, indent=2) #, ensure_ascii=False)
 
 
-###############################
+# парсинг файла с результатом запроса к HH (4) - запуск постраничного парсинга и вывод результата пользователю
 
-# word_vacancy = 'python'
-word_vacancy = input('Введите слово или фразу для поиска вакансий: ')
-# pages_nbr = 2
-pages_nbr = int(input('Сколько страниц хотите просмотреть? '))
+def hh_parser_result(f_name_req, f_name_out):
+    # word_vacancy = 'python'
+    word_vacancy = input('Введите слово или фразу для поиска вакансий: ')
+    # pages_nbr = 2
+    pages_nbr = int(input('Сколько страниц хотите просмотреть? '))
+    page_start = int(input('С какой страницы хотите начать просмотр? '))
+    i = 0
+    while i < pages_nbr:
+        page_curr = page_start - 1 + i
+        print("Страница", page_curr + 1)
+        hh_request_res = hh_request(word_vacancy, page_curr, f_name_req)
+        if hh_request_res == 404:
+            return False
+        hh_parser_res = hh_parser(f_name_req, f_name_out)
+        json_add_to_file(f_name_out, i)
+        if is_last_page(hh_request_res):  # выход, если последняя страница
+            print('Последняя страница была обработана')
+            break
+        else:
+            print(f'Количество вакансий на странице {page_curr + 1}:', len(hh_parser_res))
+        i = i + 1
+
+    with open(f"{f_name_out}.json", "r") as f:
+        hh_vacancies_full = json.load(f)
+        print()
+        print('Количество вакансий, суммарное:', len(hh_vacancies_full))
+        vacancies_nbr = int(input('Сколько вакансий показать? '))
+
+        print('Вакансии:')
+        pprint(hh_vacancies_full[0:vacancies_nbr])
+
+
+# Пред обработка (1) - задание названия файла для сохранения результатов запроса и парсинга
 
 f_name_req = "hh_vacancies_search_result"
 f_name_out = 'hh_vacancies_parsed'
 
+# Пред обработка (2) - создание файла или очистка, если он уже существует
+
 # очищаем полный файл ответа от HH
-f = open(f"{f_name_req}.rsp", 'w')
+f = open(f"{f_name_req}.rsp", 'w+')
 f.close()
 
 # очищаем полный файл json
-f = open(f"{f_name_out}.json", 'w')
+f = open(f"{f_name_out}.json", 'w+')
 f.close()
 
-print('Слово для поиска:', word_vacancy, '\nКоличество страниц для поиска:', pages_nbr)
-print()
-
-i = 0
-while i < pages_nbr:
-    print("Страница", i)
-    hh_request_res = hh_request(word_vacancy, i, f_name_req)
-    hh_parser_res = hh_parser(f_name_req, f_name_out)
-    json_add_to_file(f_name_out, i)
-    if is_last_page(hh_request_res): # выход, если последняя страница
-        print('Последняя страница была обработана')
-        break
-    else:
-        print(f'Количество вакансий на странице {i}:', len(hh_parser_res))
-    i = i + 1
-
-with open(f"{f_name_out}.json", "r") as f:
-    hh_vacancies_full = json.load(f)
-    print()
-    print('Количество вакансий, суммарное:', len(hh_vacancies_full))
-    vacancies_nbr = int(input('Сколько вакансий показать? '))
-
-    print('Вакансии:')
-    pprint(hh_vacancies_full[0:vacancies_nbr])
+# Запуск программы парсинга HH
+hh_parser_result(f_name_req, f_name_out)
 
 
-    ########### TODO TIME
-
-    # for i in range(10):
-    #     try:
-    #         answ = get(url, headers, params, proxies)
-    #         if answ.status_code != 200:
-    #             raise Exception
-    #     except Exception as e:
-    #         print(e)
-    #         time.sleep(0.5 + random.random())
-
-    ######### TODO PANDAS
+######### TODO PANDAS
